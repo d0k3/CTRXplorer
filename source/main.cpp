f94bd0f1d23ac1e9809623e6c754f2062f652cbd
d0k3 <tore.anon@gmail.com>
2016-04-06 16:28:26 +0200

    Increased version number to 0.9.6

--- source/main.cpp
+++ source/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char **argv) {
         return 0;
     }
     
-    const std::string title = "CTRX SD Explorer v0.9.5b";
+    const std::string title = "CTRX SD Explorer v0.9.6";
     const u64 tapDelay = 240;
 
     bool launcher = core::launcher();
